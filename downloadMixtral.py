de7b88bdec897ed293553cd8fb27e8afba198d62
Dylan McIntosh <69490215+dmcintosh11@users.noreply.github.com>
2024-01-08 16:43:00 -0800

    Update downloadMixtral.py

--- downloadMixtral.py
+++ downloadMixtral.py
@@ -1,3 +1,3 @@
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
-print(hf_hub_download(repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1", local_dir='/workspace/models/'))
+print(snapshot_download(repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1", local_dir='/workspace/models/', local_dir_use_symlinks=False))
