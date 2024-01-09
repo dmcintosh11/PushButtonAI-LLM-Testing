966e0a07e5dc2c3161d52a26c5379a38ca10361f
Dylan McIntosh <69490215+dmcintosh11@users.noreply.github.com>
2024-01-08 17:52:50 -0800

    Create quantize.py

---
+++ quantize.py
@@ -0,0 +1,23 @@
+from subprocess import call    
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+import sys, getopt
+import os
+
+
+
+def quantize_mod(model_path, quant_path):
+    quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+
+    # Load model
+    model = AutoAWQForCausalLM.from_pretrained(model_path, **{"low_cpu_mem_usage": True})
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    # Quantize
+    model.quantize(tokenizer, quant_config=quant_config)
+
+    #   Save quantized model
+    model.save_quantized(quant_path)
+    tokenizer.save_pretrained(quant_path)
+
+quantize_mod('/workspace/models/Mixtral-8x7B-Instruct-v0.1', '/workspace/models/Mixtral-8x7B-Instruct-v0.1-QUANT-4/')
