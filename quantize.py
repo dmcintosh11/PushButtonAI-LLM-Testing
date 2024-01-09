da9ab45f1e07a6b29648b5b33c7313c395306c82
Dylan McIntosh <69490215+dmcintosh11@users.noreply.github.com>
2024-01-08 18:18:17 -0800

    Update quantize.py

--- quantize.py
+++ quantize.py
@@ -10,7 +10,7 @@ def quantize_mod(model_path, quant_path):
     quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
 
     # Load model
-    model = AutoAWQForCausalLM.from_pretrained(model_path, **{"low_cpu_mem_usage": True})
+    model = AutoAWQForCausalLM.from_pretrained(model_path, cache_dir='/workspace/models/Mixtral-8x7B-Instruct-v0.1' **{"low_cpu_mem_usage": True})
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
     # Quantize
@@ -20,4 +20,4 @@ def quantize_mod(model_path, quant_path):
     model.save_quantized(quant_path)
     tokenizer.save_pretrained(quant_path)
 
-quantize_mod('/workspace/models/Mixtral-8x7B-Instruct-v0.1', '/workspace/models/Mixtral-8x7B-Instruct-v0.1-QUANT-4/')
+quantize_mod('mistralai/Mixtral-8x7B-Instruct-v0.1', '/workspace/models/Mixtral-8x7B-Instruct-v0.1-QUANT-4/')
